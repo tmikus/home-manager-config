4c7b9cc753ec6e4cc4dc31c4c1dde0a28a2731a7
Tomasz Mikus <mikus.tomasz@gmail.com>
2025-02-08 20:56:44 +0000

    Add a home managet configuration that's compatible with NixOS.

--- home.nix
+++ home.nix
@@ -2,19 +2,7 @@
 
 {
   imports = [
-    ./alacritty
-    ./ghostty
-    ./git.nix
-    ./intellij
-    ./java.nix
-    ./neovim.nix
-    ./overlays.nix
-    ./starship.nix
-    ./tmux.nix
-    ./wezterm
-    ./zed
-    ./zoxide.nix
-    ./zsh.nix
+    ./configuration.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
@@ -32,94 +20,4 @@
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages =
-    with pkgs;
-    [
-      # # Adds the 'hello' command to your environment. It prints a friendly
-      # # "Hello, world!" when run.
-      # pkgs.hello
-      # # It is sometimes useful to fine-tune packages, for example, by applying
-      # # overrides. You can do that directly here, just don't forget the
-      # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-      # # fonts?
-      # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-      # # You can also create simple shell scripts directly inside your
-      # # configuration. For example, this adds a command 'my-hello' to your
-      # # environment:
-      # (pkgs.writeShellScriptBin "my-hello" ''
-      #   echo "Hello, ${config.home.username}!"
-      # '')
-      bat
-      delta
-      deno
-      direnv
-      fzf
-      go
-      jq
-      lua-language-server
-      mise
-      nil
-      nixd
-      nixfmt-rfc-style
-      ripgrep
-      rustup
-      starship
-      thefuck
-      tree-sitter
-      unzip
-      xclip
-      wget
-      zig
-    ]
-    ++ (
-      if pkgs.stdenv.hostPlatform.isDarwin then
-        [ ]
-      else
-        with pkgs;
-        [
-          clang
-          gnumake
-          plocate
-        ]
-    );
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/tmikus/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    EDITOR = "nvim";
-    GOPROXY = "direct";
-  };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager = {
-    enable = true;
-  };
 }

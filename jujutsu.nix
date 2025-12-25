45267e22a3412177f5d9848446f5fdaba9907b4e
Tomasz Mikus <tomasz@mikus.uk>
2025-12-25 12:05:48 +0000

    Add 'jj-spr'.
    Created using jj-spr 0.1.0

--- jujutsu.nix
+++ jujutsu.nix
@@ -2,12 +2,15 @@
   programs.jujutsu = {
     enable = true;
     settings = {
+      aliases = {
+        spr = ["util" "exec" "--" "jj-spr"];
+      };
       user = {
         name = "Tomasz Mikus";
         email = "tomasz@mikus.uk";
       };
-      aliases = {
-        spr = ["util" "exec" "--" "jj-spr"];
+      spr = {
+        branchPrefix = "tmikus-";
       };
       ui = {
         default-command = "log";

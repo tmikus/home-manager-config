530c718380cf01065164ba654b402fa8107f725c
Tomasz Mikus <tomasz@mikus.uk>
2025-12-26 14:52:59 +0000

    [jj-spr] initial version
    Created using jj-spr 0.1.0

--- jujutsu.nix
+++ jujutsu.nix
@@ -3,6 +3,8 @@
     enable = true;
     settings = {
       aliases = {
+        l = ["log" "--no-pager" "--limit" "10"];
+        rebase_main = ["jj" "rebase" "-r" "@" "-d" "main@origin"];
         spr = ["util" "exec" "--" "jj-spr"];
       };
       user = {

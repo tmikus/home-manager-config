91c69a8ceafb013e2bcfba07d6e89d90fa763064
Tomasz Mikuś <hi@tomaszmik.us>
2026-01-11 17:17:42 +0000

    Cleanup JJ configuration.
    Reviewers: 
    
    Pull Request: https://github.com/tmikus/home-manager-config/pull/21

--- fish.nix
+++ fish.nix
@@ -1,6 +1,29 @@
 { lib, pkgs, ... }:
 
 {
+  # Use home-manager's built-in direnv integration
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
+  # Declarative environment variables
+  home.sessionVariables = {
+    GOPATH = "$HOME/go";
+  };
+
+  # Declarative PATH management
+  home.sessionPath = [
+    "$HOME/.toolbox/bin"
+    "$HOME/.cargo/bin"
+    "$HOME/.local/bin"
+    "$HOME/.jetbrains"
+    "/usr/local/bin"
+    "$HOME/go/bin"
+    "$HOME/Library/Android/sdk/platform-tools"
+    "$HOME/.local/share/mise/shims"
+  ];
+
   programs.fish = {
     enable = true;
 
@@ -12,9 +35,9 @@
 
       # If nix-daemon command does not exist, then prompt the user to run the ./restore-nix.sh script
       if not command -v nix-daemon > /dev/null
-        set RED '\033[0;31m'
-        set NC '\033[0m'
-        echo -e "$RED""ERROR: Nix and Home Manager command not found.""$NC"
+        set_color red
+        echo "ERROR: Nix and Home Manager command not found."
+        set_color normal
         echo "    Looks like the latest version of MacOS has changed the /etc/zshrc file."
         echo "    Please run the following script to restore Nix and Home Manager."
         echo ""
@@ -24,16 +47,6 @@
     '';
 
     loginShellInit = ''
-      # Add toolbox to PATH
-      set -gx GOPATH "$HOME/go"
-      fish_add_path -g "$HOME/.toolbox/bin"
-      fish_add_path -g "$HOME/.cargo/bin"
-      fish_add_path -g "$HOME/.local/bin"
-      fish_add_path -g "$HOME/.jetbrains"
-      fish_add_path -g "/usr/local/bin"
-      fish_add_path -g "$GOPATH/bin"
-      fish_add_path -g "$HOME/Library/Android/sdk/platform-tools"
-
       # Set PATH, MANPATH, etc., for Homebrew.
       if test -f /opt/homebrew/bin/brew
         eval (/opt/homebrew/bin/brew shellenv)
@@ -41,8 +54,6 @@
     '';
 
     interactiveShellInit = ''
-      set -g fish_greeting
-
       # Only set theme if not already set to ayu
       if test "$__fish_theme" != "ayu"
         fish_config theme choose ayu
@@ -54,14 +65,9 @@
         source "$HOME/.local/share/mechanic/complete.fish"
       end
 
-      # Use mise shims for faster startup
-      fish_add_path -g "$HOME/.local/share/mise/shims"
+      # Load mise completions
       if test -f "$HOME/.local/share/mise/completions.fish"
         source "$HOME/.local/share/mise/completions.fish"
-      end
-
-      if command -v direnv > /dev/null
-        direnv hook fish | source
       end
 
       # Add Ghostty integration
@@ -75,32 +81,43 @@
       end
     '';
 
-    shellAliases = lib.mkMerge [
-      {
-        bb = "brazil-build $argv";
-        bbb = "brazil-recursive-cmd --allPackages brazil-build $argv";
-        bws = "brazil workspace $argv";
-        bws_reset = "brazil-recursive-cmd --allPackages \"git checkout mainline && git reset --hard origin/mainline\"";
-        cat = "bat";
-        ls = "eza";
-        ll = "ls -l";
-        l = "ls -l";
-        la = "ls -a";
-        reset_nvim = "rm -rf ~/.local/share/nvim ~/.local/state/nvim ~/.config/nvim ~/.cache/nvim";
-        update_home_manager = "nix-channel --update && home-manager switch";
-      }
-      (
-        if pkgs.stdenv.hostPlatform.isDarwin then
-          {
-            morning = "ssh-add -D && mwinit -f && ssh-add --apple-use-keychain ~/.ssh/id_ecdsa";
-            update_db = "sudo /usr/libexec/locate.updatedb";
-          }
-        else
-          {
-            morning = "mwinit -o";
-            update_db = "sudo updatedb";
-          }
-      )
-    ];
+    # Use functions for commands that need arguments
+    functions = {
+      # Disable greeting
+      fish_greeting = "";
+      # Brazil build helpers that support arguments
+      bbb = "brazil-recursive-cmd --allPackages brazil-build $argv";
+      bws_reset = ''brazil-recursive-cmd --allPackages "git checkout mainline && git reset --hard origin/mainline"'';
+    };
+
+    # Use abbreviations for common commands (expand inline, visible in history)
+    shellAbbrs = {
+      bb = "brazil-build";
+      bws = "brazil workspace";
+      g = "git";
+      gc = "git commit";
+      gp = "git push";
+      gl = "git log";
+      gs = "git status";
+      hm = "home-manager";
+    };
+
+    # Simple aliases for command substitutions
+    shellAliases = {
+      cat = "bat";
+      ls = "eza";
+      ll = "eza -l";
+      l = "eza -l";
+      la = "eza -a";
+      lla = "eza -la";
+      reset_nvim = "rm -rf ~/.local/share/nvim ~/.local/state/nvim ~/.config/nvim ~/.cache/nvim";
+      update_home_manager = "nix-channel --update && home-manager switch";
+    } // lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
+      morning = "ssh-add -D && mwinit -f && ssh-add --apple-use-keychain ~/.ssh/id_ecdsa";
+      update_db = "sudo /usr/libexec/locate.updatedb";
+    } // lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
+      morning = "mwinit -o";
+      update_db = "sudo updatedb";
+    };
   };
 }
--- jujutsu.nix
+++ jujutsu.nix
@@ -4,8 +4,15 @@
     settings = {
       aliases = {
         l = ["log" "--no-pager" "--limit" "10"];
-        rebase_main = ["rebase" "-r" "@" "-d" "main@origin"];
+        ll = ["log" "--limit" "30"];
+        la = ["log" "-r" "all()"];
+        s = ["status"];
+        d = ["diff"];
+        ds = ["diff" "--stat"];
         spr = ["util" "exec" "--" "jj-spr"];
+        land = ["util" "exec" "--" "sh" "-c" "jj spr land -r @- && jj git fetch && jj rebase -r @ -d main@origin"];
+        undo = ["op" "undo"];
+        evolog = ["evolog" "--no-pager" "--limit" "10"];
       };
       user = {
         name = "Tomasz Mikus";
@@ -14,26 +21,37 @@
       spr = {
         branchPrefix = "tmikus-";
       };
+      git = {
+        auto-local-bookmark = true;
+        push-bookmark-prefix = "push-";
+      };
+      snapshot = {
+        max-new-file-size = "10MiB";
+      };
       ui = {
         default-command = "l";
         editor = "nvim";
-        # Enable pagination for commands that support it (default)
         paginate = "auto";
-
-        # delta
         pager = "delta";
         diff-formatter = ":git";
         delta = {
           navigate = true;
         };
       };
+      merge-tools = {
+        nvim = {
+          program = "nvim";
+          merge-args = ["-d" "$left" "$base" "$right" "$output" "-c" "wincmd J"];
+        };
+      };
       revset-aliases = {
-        # Define trunk as main@origin
         "trunk()" = "main@origin";
+        "mine()" = ''author(exact:"tomasz@mikus.uk")'';
+        "wip()" = ''description(exact:"") & mine()'';
+        "stack()" = "ancestors(@, 10) & mine()";
       };
       revsets = {
-        # Show all revisions in log by default (no elided revisions)
-        log = "::";
+        log = "present(@) | ancestors(immutable_heads().., 2) | present(trunk())";
       };
     };
   };

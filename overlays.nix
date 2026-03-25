939133333c4307ecfffc56767c99469de913be62
Tomasz Mikus <tmikus@twitch.tv>
2026-03-25 11:01:15 +0000

    Address a problem with direnv.

--- overlays.nix
+++ overlays.nix
@@ -4,6 +4,11 @@
       jj-spr = prev.callPackage ./packages/jj-spr { };
       nv-tmikus = prev.callPackage ./packages/nv-tmikus { };
 
+      # Fix direnv build: CGO is required for -linkmode=external
+      direnv = prev.direnv.overrideAttrs (old: {
+        env = (old.env or { }) // { CGO_ENABLED = 1; };
+      });
+
       # Fix for the tmux not opening ZSH: https://github.com/nix-community/home-manager/issues/5952
       tmuxPlugins = prev.tmuxPlugins // {
         sensible = prev.tmuxPlugins.sensible.overrideAttrs (prev: {
